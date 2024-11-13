87b18e98d90eb1a29dc687f2163b237db3946b36
Stanley Wintergreen <kepocnhh@protonmail.com>
2024-11-13 14:42:10 +0300

    init android

---
+++ app/src/main/kotlin/test/android/widget/App.kt
@@ -0,0 +1,10 @@
+package test.android.widget
+
+import android.app.Application
+
+internal class App : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        // todo
+    }
+}
